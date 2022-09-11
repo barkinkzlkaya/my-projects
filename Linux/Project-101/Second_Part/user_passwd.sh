36640a27244ed2c8fa5e16e5c1900643a4dcb99d
Barkın <109108512+barkinkzlkaya@users.noreply.github.com>
2022-09-11 16:38:19 +0300

    Second Task Done

--- Linux/Project-101/Second_Part/user_passwd.sh
+++ Linux/Project-101/Second_Part/user_passwd.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-#
-# This script creates a new user on the local system.
-# You will be prompted to enter the username (login), the person name, and a password.
-# The username, password, and host for the account will be displayed.
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
-# Make sure the script is being executed with superuser privileges.
-
-
-# Get the username (login).
-
-# Get the real name (contents for the description field).
-
-# Get the password.
-
-# Create the account.
-
-# Check to see if the useradd command succeeded.
-# We don't want to tell the user that an account was created when it hasn't been.
-
-# Set the password.
-
-# Check to see if the passwd command succeeded.
-
-# Force password change on first login.
-
-# Display the username, password, and the host where the user was created.
-
+read -p "Enter the username (login): " USER_NAME
+read -p "Enter the real name (first and last name): " COMMENT
+read -p "Enter the password: " PASSWORD
+useradd -c "${COMMENT}" -m ${USER_NAME}
+if [ $? -eq 0 ]; then
+    echo "User account created."
+else
+    echo "User account not created."
+    exit 1
+fi
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+if [ $? -eq 0 ]; then
+    echo "Password set for user ${USER_NAME}."
+else
+    echo "Password not set for user ${USER_NAME}."
+    exit 2
+fi
+passwd -e ${USER_NAME}
+echo "Username: ${USER_NAME}"
+echo "Host: ${HOSTNAME}"
+exit 0
